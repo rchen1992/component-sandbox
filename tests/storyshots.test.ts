71c21aa660fa7ef09eba13230836e4ac322a85e2
Raymond Chen <raymoc2@uci.edu>
2018-11-11 12:00:52 -0800

    jest styled components for seeing CSS in storyshots

--- tests/storyshots.test.ts
+++ tests/storyshots.test.ts
@@ -1,3 +1,15 @@
 import initStoryshots from '@storybook/addon-storyshots';
+import 'jest-styled-components';
+import * as renderer from 'react-test-renderer';
 
-initStoryshots({});
+// Using this function means we'll see the change in the css in the snapshot
+// diff instead of just the change in classname
+const styledSnapshot = (params: any) => {
+    const storyElement = params.story.render(params.context);
+    const tree = renderer.create(storyElement).toJSON();
+    expect(tree).toMatchSnapshot();
+};
+
+initStoryshots({
+    test: styledSnapshot,
+});
