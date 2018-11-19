8195284130305c670eee78755a23903adcbc4f2a
Raymond Chen <raymoc2@uci.edu>
2018-11-18 23:05:20 -0800

    column positioning tests

--- src/Grid/Grid.test.tsx
+++ src/Grid/Grid.test.tsx
@@ -3,6 +3,7 @@ import { cleanup, render } from 'react-testing-library';
 import { Row, Col } from './index';
 import { expectRenderError, expectConsoleError } from '../../tests/testUtils';
 import { GRID_COLUMN_MAX } from './util';
+import 'jest-styled-components';
 
 afterEach(cleanup);
 
@@ -32,5 +33,61 @@ describe('Grid', () => {
     test('should be able to apply style to Row', () => {
         const { container } = render(<Row style={{ backgroundColor: 'red' }} />);
         expect(container.innerHTML).toMatch('background-color: red');
+    });
+
+    describe('Column positioning', () => {
+        test('a Row and Col with no props should span the full grid', () => {
+            const { getByTestId } = render(
+                <Row>
+                    <Col data-testid="col" />
+                </Row>
+            );
+
+            const col = getByTestId('col');
+            expect(col).toHaveStyleRule('grid-column-start', '1');
+            expect(col).toHaveStyleRule('grid-column-end', 'span 24');
+        });
+
+        test('should render Cols in correct position based on span', () => {
+            const { getByTestId } = render(
+                <Row>
+                    <Col data-testid="col1" span={6} />
+                    <Col data-testid="col2" span={8} />
+                    <Col data-testid="col3" span={10} />
+                </Row>
+            );
+
+            const col1 = getByTestId('col1');
+            const col2 = getByTestId('col2');
+            const col3 = getByTestId('col3');
+
+            expect(col1).toHaveStyleRule('grid-column-start', '1');
+            expect(col1).toHaveStyleRule('grid-column-end', 'span 6');
+            expect(col2).toHaveStyleRule('grid-column-start', '7');
+            expect(col2).toHaveStyleRule('grid-column-end', 'span 8');
+            expect(col3).toHaveStyleRule('grid-column-start', '15');
+            expect(col3).toHaveStyleRule('grid-column-end', 'span 10');
+        });
+
+        test('should render Cols in correct position based on span and offset', () => {
+            const { getByTestId } = render(
+                <Row>
+                    <Col data-testid="col1" span={4} offset={4} />
+                    <Col data-testid="col2" span={4} offset={6} />
+                    <Col data-testid="col3" span={4} offset={2} />
+                </Row>
+            );
+
+            const col1 = getByTestId('col1');
+            const col2 = getByTestId('col2');
+            const col3 = getByTestId('col3');
+
+            expect(col1).toHaveStyleRule('grid-column-start', '5');
+            expect(col1).toHaveStyleRule('grid-column-end', 'span 4');
+            expect(col2).toHaveStyleRule('grid-column-start', '15');
+            expect(col2).toHaveStyleRule('grid-column-end', 'span 4');
+            expect(col3).toHaveStyleRule('grid-column-start', '21');
+            expect(col3).toHaveStyleRule('grid-column-end', 'span 4');
+        });
     });
 });
