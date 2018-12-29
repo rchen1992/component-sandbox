1c273c345f4bbab04febd10b2a3350359413f0b5
Raymond Chen <raymoc2@uci.edu>
2018-12-29 11:54:16 -0800

    checkbox onChange story

--- stories/Checkbox.stories.tsx
+++ stories/Checkbox.stories.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import Checkbox from '../src/Checkbox';
 import { wInfo } from '../src/utils';
+import { action } from '@storybook/addon-actions';
 // import { action } from '@storybook/addon-actions';
 
 const stories = storiesOf('Components/Checkbox', module) as any;
@@ -95,6 +96,47 @@ stories.add(
         ~~~js
         <Checkbox indeterminate>Option 1</Checkbox>
         <Checkbox indeterminate disabled >Option 2</Checkbox>
+        ~~~`)
+);
+
+/**
+|--------------------------------------------------
+| onChange
+|--------------------------------------------------
+*/
+stories.add(
+    'onChange',
+    () => (
+        <Checkbox
+            value="option1"
+            onChange={(e, data) => {
+                console.log(data);
+                action('onChange')(e);
+            }}
+        >
+            Option 1
+        </Checkbox>
+    ),
+    wInfo(`
+        ### Notes
+
+        You can attach an **\`onChange\`** handler to the input.
+
+        The first parameter for the handler will be the React change event.
+        
+        The second parameter will be a **\`data\`** payload containing **\`prevChecked\`** and the input **\`value\`**.
+
+        ### Usage
+        ~~~js
+        <Checkbox
+            value="option1"
+            onChange={(e, data) => {
+                console.log(data);
+                action('onChange')(e);
+            }}
+        >
+            Option 1
+        </Checkbox>
         ~~~`)
 );
 
