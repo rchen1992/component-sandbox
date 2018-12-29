f1d7a2c9030c37c4b8f51eedd403d71d742b23dd
Raymond Chen <rchen@squareup.com>
2018-12-29 12:38:32 -0800

    checkbox check all story

--- stories/Checkbox.stories.tsx
+++ stories/Checkbox.stories.tsx
@@ -123,7 +123,7 @@ stories.add(
         You can attach an **\`onChange\`** handler to the input.
 
         The first parameter for the handler will be the React change event.
-        
+
         The second parameter will be a **\`data\`** payload containing **\`prevChecked\`** and the input **\`value\`**.
 
         ### Usage
@@ -211,6 +211,108 @@ stories.add(
         };
         ~~~
         `)
+);
+
+/**
+|--------------------------------------------------
+| Check all
+|--------------------------------------------------
+*/
+const CheckAll = () => {
+    const [fruits] = React.useState(['Apple', 'Orange', 'Pear', 'Mango']);
+    const [checkAll, setCheckAll] = React.useState(false);
+    const [checkedFruits, setCheckedFruits] = React.useState(['Apple', 'Orange']);
+    const [isIndeterminate, setIsIndeterminate] = React.useState(true);
+
+    function onCheckAllChange(e: React.ChangeEvent, data: any) {
+        const checkedFruits = !data.prevChecked ? fruits : [];
+
+        setIsIndeterminate(false);
+        setCheckAll(!data.prevChecked);
+        setCheckedFruits(checkedFruits);
+    }
+
+    function onCheckboxGroupChange(value: string[]) {
+        const checkedCount = value.length;
+
+        setCheckedFruits(value);
+        setCheckAll(checkedCount === fruits.length);
+        setIsIndeterminate(checkedCount > 0 && checkedCount < fruits.length);
+    }
+
+    return (
+        <>
+            <Checkbox
+                checked={checkAll}
+                indeterminate={isIndeterminate}
+                onChange={onCheckAllChange}
+                style={{ marginBottom: '10px' }}
+            >
+                Check all
+            </Checkbox>
+            <Checkbox.Group value={checkedFruits} onChange={onCheckboxGroupChange}>
+                {fruits.map((fruit: string) => (
+                    <Checkbox key={fruit} value={fruit} style={formatStyles}>
+                        {fruit}
+                    </Checkbox>
+                ))}
+            </Checkbox.Group>
+        </>
+    );
+};
+
+stories.add(
+    'Check all',
+    () => <CheckAll />,
+    wInfo(`
+        ### Notes
+
+        Example of implementing "check all" functionality.
+        
+        ### Usage
+        ~~~js
+        const CheckAll = () => {
+            const [fruits] = React.useState(['Apple', 'Orange', 'Pear', 'Mango']);
+            const [checkAll, setCheckAll] = React.useState(false);
+            const [checkedFruits, setCheckedFruits] = React.useState(['Apple', 'Orange']);
+            const [isIndeterminate, setIsIndeterminate] = React.useState(true);
+
+            function onCheckAllChange(e, data) {
+                const checkedFruits = !data.prevChecked ? fruits : [];
+
+                setIsIndeterminate(false);
+                setCheckAll(!data.prevChecked);
+                setCheckedFruits(checkedFruits);
+            }
+
+            function onCheckboxGroupChange(value) {
+                const checkedCount = value.length;
+
+                setCheckedFruits(value);
+                setCheckAll(checkedCount === fruits.length);
+                setIsIndeterminate(checkedCount > 0 && checkedCount < fruits.length);
+            }
+
+            return (
+                <>
+                    <Checkbox
+                        checked={checkAll}
+                        indeterminate={isIndeterminate}
+                        onChange={onCheckAllChange}
+                    >
+                        Check all
+                    </Checkbox>
+                    <Checkbox.Group value={checkedFruits} onChange={onCheckboxGroupChange}>
+                        {fruits.map((fruit) => (
+                            <Checkbox key={fruit} value={fruit}>
+                                {fruit}
+                            </Checkbox>
+                        ))}
+                    </Checkbox.Group>
+                </>
+            );
+        };
+        ~~~`)
 );
 
 export default stories;
