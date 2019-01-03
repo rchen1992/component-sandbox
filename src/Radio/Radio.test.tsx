2297530ddf2cc011ca02aae6623bcd37afa3c531
Raymond Chen <raymoc2@uci.edu>
2019-01-02 16:44:18 -0800

    radio basic tests

--- src/Radio/Radio.test.tsx
+++ src/Radio/Radio.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { cleanup } from 'react-testing-library';
+import { render, cleanup, fireEvent } from 'react-testing-library';
 import Radio from './index';
 import 'jest-styled-components';
 import { testComponentCanHandleStyles } from '../../tests/testUtils';
@@ -8,4 +8,40 @@ afterEach(cleanup);
 
 describe('Checkbox', () => {
     testComponentCanHandleStyles(<Radio />);
+
+    test('should be able to render a radio button with checked input', () => {
+        const { container } = render(<Radio checked={true}>Option</Radio>);
+        const input = container.querySelector('input') as HTMLInputElement;
+        expect(input.checked).toBeTruthy();
+    });
+
+    test('should be able to render radio button without checked input', () => {
+        const { container } = render(<Radio checked={false}>Option</Radio>);
+        const input = container.querySelector('input') as HTMLInputElement;
+        expect(input.checked).toBeFalsy();
+    });
+
+    test('should be able to pass value to radio input', () => {
+        const value = 'option';
+        const { container } = render(<Radio value={value}>Option</Radio>);
+
+        const input = container.querySelector('input') as HTMLInputElement;
+        expect(input.value).toBe(value);
+    });
+
+    test('should be able to attach onChange handler that provides input value', () => {
+        let mockValue;
+        const value = 'option';
+        const onChange = jest.fn().mockImplementation((e, data) => (mockValue = data.value));
+        const { container } = render(
+            <Radio value={value} onChange={onChange}>
+                Option
+            </Radio>
+        );
+
+        fireEvent.click(container.firstElementChild as Element);
+
+        expect(onChange).toHaveBeenCalledTimes(1);
+        expect(mockValue).toBe(value);
+    });
 });
