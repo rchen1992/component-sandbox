f4b778ca6cd41314e239ea8ef536e8adcd02652f
Raymond Chen <raymoc2@uci.edu>
2019-01-05 11:52:54 -0800

    input sizes

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,9 +1,47 @@
 // import * as React from 'react';
-import styled from '../sc-utils';
+import styled, { css, ITheme } from '../sc-utils';
+import { InterpolationValue } from 'styled-components';
 
 interface IInputProps {
     disabled?: boolean;
+    inputSize?: string;
 }
+
+export enum InputSize {
+    large = 'large',
+    small = 'small',
+    mini = 'mini',
+}
+
+const InputSizeCss = (props: IInputProps & { theme: ITheme }): InterpolationValue[] | undefined => {
+    if (props.theme && props.inputSize) {
+        switch (props.inputSize) {
+            case InputSize.large:
+                return css`
+                    height: 42px;
+                    font-size: 16px;
+                `;
+            case InputSize.small:
+                return css`
+                    height: 30px;
+                    font-size: 13px;
+                `;
+            case InputSize.mini:
+                return css`
+                    height: 22px;
+                    font-size: 12px;
+                `;
+            default:
+                console.error(`Input size \`${props.inputSize}\` is not a valid input size.`);
+                return undefined;
+        }
+    }
+
+    return css`
+        height: 36px;
+        font-size: 14px;
+    `;
+};
 
 export default styled<IInputProps, 'input'>('input')`
     /* Remove native input css */
@@ -16,15 +54,16 @@ export default styled<IInputProps, 'input'>('input')`
         ${props => (props.disabled ? props.theme.infoColorAccent : props.theme.defaultBorderColor)};
     border-radius: 4px;
     box-sizing: border-box;
-    height: 36px;
     padding: 3px 10px;
     outline: none;
     line-height: 1;
-    font-size: 14px;
     width: 180px;
     color: ${props => props.theme.defaultTextColor};
     transition: border-color 200ms;
     cursor: ${props => (props.disabled ? 'not-allowed' : 'initial')};
+
+    /* Resolve input size */
+    ${InputSizeCss}
 
     ::placeholder {
         /* Chrome, Firefox, Opera, Safari 10.1+ */
--- stories/Input.stories.tsx
+++ stories/Input.stories.tsx
@@ -6,9 +6,9 @@ import { wInfo } from '../src/utils';
 
 const stories = storiesOf('Components/Input', module) as any;
 
-// const formatStyles = {
-//     marginRight: '8px',
-// };
+const formatStyles = {
+    marginRight: '8px',
+};
 
 /**
 |--------------------------------------------------
@@ -45,6 +45,37 @@ stories.add(
         ### Usage
         ~~~js
         <Input placeholder="Please input" disabled />
+        ~~~`)
+);
+
+/**
+|--------------------------------------------------
+| Sizes
+|--------------------------------------------------
+*/
+stories.add(
+    'Sizes',
+    () => (
+        <>
+            <Input placeholder="Please input" style={formatStyles} inputSize="large" />
+            <Input placeholder="Please input" style={formatStyles} />
+            <Input placeholder="Please input" style={formatStyles} inputSize="small" />
+            <Input placeholder="Please input" inputSize="mini" />
+        </>
+    ),
+    wInfo(`
+        ### Notes
+
+        Use **\`inputSize\`** to set input sizes.
+
+        Available sizes (other than default) are **\`large\`**, **\`small\`**, and **\`mini\`**.
+        
+        ### Usage
+        ~~~js
+        <Input placeholder="Please input" inputSize="large" />
+        <Input placeholder="Please input" />
+        <Input placeholder="Please input" inputSize="small" />
+        <Input placeholder="Please input" inputSize="mini" />
         ~~~`)
 );
 
