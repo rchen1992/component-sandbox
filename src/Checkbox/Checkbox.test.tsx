91d2608853f88996776ef66d148ecbb84ea7c470
Raymond Chen <rchen1992@gmail.com>
2019-01-27 16:31:24 -0800

    Checkbox component: move checkbox group to separate file.

--- src/Checkbox/Checkbox.test.tsx
+++ src/Checkbox/Checkbox.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { cleanup, render, fireEvent } from 'react-testing-library';
 import Checkbox from './index';
 import 'jest-styled-components';
-import { testComponentCanHandleStyles, expectRenderError } from '../../tests/testUtils';
+import { testComponentCanHandleStyles } from '../../tests/testUtils';
 
 afterEach(cleanup);
 
@@ -101,126 +101,5 @@ describe('Checkbox', () => {
 
         expect(onChange).toHaveBeenCalledTimes(1);
         expect(mockData).toMatchObject({ prevChecked: false, value });
-    });
-
-    describe('Checkbox Group', () => {
-        test('should throw error when trying to render a child element that is not a checkbox', () => {
-            const element = (
-                <Checkbox.Group>
-                    <Checkbox>Option 1</Checkbox>
-                    <span>hello world</span>
-                </Checkbox.Group>
-            );
-
-            expectRenderError(
-                element,
-                'The only valid child to a Checkbox Group element is a Checkbox element.'
-            );
-        });
-
-        test('should render child checkboxes as checked when their values are in the value array of the checkbox group', () => {
-            const checkedBoxes = ['option2', 'option5'];
-            const uncheckedBoxes = ['option1', 'option3', 'option4'];
-
-            const { getByText } = render(
-                <Checkbox.Group value={checkedBoxes}>
-                    <Checkbox value="option1">option1</Checkbox>
-                    <Checkbox value="option2">option2</Checkbox>
-                    <Checkbox value="option3">option3</Checkbox>
-                    <Checkbox value="option4">option4</Checkbox>
-                    <Checkbox value="option5">option5</Checkbox>
-                </Checkbox.Group>
-            );
-
-            uncheckedBoxes.forEach(value => {
-                let option = getByText(value);
-                let input = option.nextSibling as HTMLInputElement;
-                expect(input.checked).toBe(false);
-            });
-
-            checkedBoxes.forEach(value => {
-                let option = getByText(value);
-                let input = option.nextSibling as HTMLInputElement;
-                expect(input.checked).toBe(true);
-            });
-        });
-
-        test('should be able to attach onChange handler that provides updated value list', () => {
-            let mockValue;
-            const onChange = jest.fn().mockImplementation(value => {
-                mockValue = value;
-            });
-            const checkedBoxes = ['option2', 'option5'];
-
-            const { getByLabelText } = render(
-                <Checkbox.Group value={checkedBoxes} onChange={onChange}>
-                    <Checkbox value="option1">option1</Checkbox>
-                    <Checkbox value="option2">option2</Checkbox>
-                    <Checkbox value="option3">option3</Checkbox>
-                    <Checkbox value="option4">option4</Checkbox>
-                    <Checkbox value="option5">option5</Checkbox>
-                </Checkbox.Group>
-            );
-
-            // Click on the first checkbox
-            let option1 = getByLabelText('option1');
-            fireEvent.click(option1);
-
-            // onChange handler should fire with updated value list
-            expect(onChange).toHaveBeenCalledTimes(1);
-            expect(mockValue).toEqual([...checkedBoxes, 'option1']);
-        });
-
-        test('should be able to use min prop to enforce minimum number of checked boxes', () => {
-            let mockValue;
-            const onChange = jest.fn().mockImplementation(value => {
-                mockValue = value;
-            });
-            const checkedBoxes = ['option2', 'option5'];
-
-            const { getByLabelText } = render(
-                <Checkbox.Group min={2} value={checkedBoxes} onChange={onChange}>
-                    <Checkbox value="option1">option1</Checkbox>
-                    <Checkbox value="option2">option2</Checkbox>
-                    <Checkbox value="option3">option3</Checkbox>
-                    <Checkbox value="option4">option4</Checkbox>
-                    <Checkbox value="option5">option5</Checkbox>
-                </Checkbox.Group>
-            );
-
-            // Try to uncheck one of the checked boxes
-            let option = getByLabelText('option2');
-            fireEvent.click(option);
-
-            // onChange handler should fire with value list that is unchanged
-            expect(onChange).toHaveBeenCalledTimes(1);
-            expect(mockValue).toEqual(checkedBoxes);
-        });
-
-        test('should be able to use max prop to enforce maximum number of checked boxes', () => {
-            let mockValue;
-            const onChange = jest.fn().mockImplementation(value => {
-                mockValue = value;
-            });
-            const checkedBoxes = ['option2', 'option5'];
-
-            const { getByLabelText } = render(
-                <Checkbox.Group max={2} value={checkedBoxes} onChange={onChange}>
-                    <Checkbox value="option1">option1</Checkbox>
-                    <Checkbox value="option2">option2</Checkbox>
-                    <Checkbox value="option3">option3</Checkbox>
-                    <Checkbox value="option4">option4</Checkbox>
-                    <Checkbox value="option5">option5</Checkbox>
-                </Checkbox.Group>
-            );
-
-            // Try to check one of the unchecked boxes
-            let option = getByLabelText('option1');
-            fireEvent.click(option);
-
-            // onChange handler should fire with value list that is unchanged
-            expect(onChange).toHaveBeenCalledTimes(1);
-            expect(mockValue).toEqual(checkedBoxes);
-        });
     });
 });
--- src/Checkbox/Checkbox.tsx
+++ src/Checkbox/Checkbox.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import styled, { IWithStyles, css } from '../sc-utils';
-import { IChangeHandlerWithData } from 'types';
+import { IChangeHandlerWithData } from '../types';
+import CheckboxGroup, { ICheckboxGroupProps } from './CheckboxGroup';
 
-interface ICheckboxProps extends IWithStyles {
+export interface ICheckboxProps extends IWithStyles {
     children?: React.ReactNode;
     defaultChecked?: boolean;
     checked?: boolean;
@@ -12,16 +13,9 @@ interface ICheckboxProps extends IWithStyles {
     onChange?: IChangeHandlerWithData<ICheckboxData>;
 }
 
-interface ICheckboxData {
+export interface ICheckboxData {
     prevChecked: boolean;
     value?: string;
-}
-
-interface ICheckboxGroupProps {
-    value?: string[];
-    min?: number;
-    max?: number;
-    onChange?: (value: string[]) => void;
 }
 
 type CheckboxWithRef = React.ForwardRefExoticComponent<ICheckboxProps & React.RefAttributes<any>>;
@@ -160,81 +154,10 @@ const Checkbox = React.forwardRef<any, ICheckboxProps>((props, ref) => {
     );
 });
 
-/**
-|--------------------------------------------------
-| Checkbox Group
-|--------------------------------------------------
-*/
 const CheckboxWithGroup = Checkbox as CheckboxWithRef & {
     Group: React.FunctionComponent<ICheckboxGroupProps>;
 };
 
-CheckboxWithGroup.Group = props => {
-    function onChange(e: React.ChangeEvent, data: ICheckboxData) {
-        if (!props.onChange) {
-            return;
-        }
-
-        let newValue = props.value || [];
-
-        // If checkbox doesn't have a value, simply call onChange with old value list.
-        if (!data.value) {
-            props.onChange(newValue);
-            return;
-        }
-
-        // Length restrictions for value list
-        const max = props.max !== undefined ? props.max : Number.MAX_SAFE_INTEGER;
-        const min = props.min !== undefined ? props.min : 0;
-
-        /**
-         * Calculate the new value list so we can pass it to the onChange prop.
-         *
-         * If checkbox is currently checked, we want to uncheck it.
-         * As long as we are above the minimum number of checked boxes, remove it from value list.
-         */
-        if (data.prevChecked && newValue.includes(data.value) && newValue.length > min) {
-            newValue = newValue.filter(val => val !== data.value);
-        } else if (!data.prevChecked && !newValue.includes[data.value] && newValue.length < max) {
-            // Otherwise, if it is currently unchecked, we want to check it.
-            // As long as we are under the maximum number of checked boxes, add it to value list.
-            newValue = [...newValue, data.value as string];
-        }
-
-        props.onChange(newValue);
-    }
-
-    const children = React.Children.map(props.children, (child, index) => {
-        if (!React.isValidElement(child) || child.type !== Checkbox) {
-            throw new Error(
-                'The only valid child to a Checkbox Group element is a Checkbox element.'
-            );
-        }
-
-        let checkbox = child as React.ReactElement<ICheckboxProps>;
-        if (!props.value) {
-            return child;
-        }
-
-        /**
-         * Check for checkbox value. If undefined, default it to the child's index.
-         */
-        let checkboxValue = checkbox.props.value;
-        if (checkboxValue === undefined) {
-            console.error(
-                'Every child of a checkbox group should have a `value` prop. If not specified, it will default to the index of the child.'
-            );
-            checkboxValue = index.toString();
-        }
-
-        return React.cloneElement(checkbox, {
-            value: checkboxValue,
-            checked: props.value.includes(checkboxValue),
-            onChange,
-        });
-    });
-
-    return <div>{children}</div>;
-};
+CheckboxWithGroup.Group = CheckboxGroup;
 
 export default CheckboxWithGroup;
