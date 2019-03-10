4c0f398971f0bec18f169b9eaaf1b9046f16df0d
Raymond Chen <rchen1992@gmail.com>
2019-03-09 21:27:13 -0800

    Update about page.

--- .storybook/welcomeStory.js
+++ .storybook/welcomeStory.js
@@ -4,22 +4,21 @@ import { storiesOf } from '@storybook/react';
 import { wInfo } from './utils';
 
 storiesOf('Welcome', module).add(
-    'to your new Storybook🎊',
-    () => <div>This is an example component</div>,
+    'About this project',
+    () => <div />,
     wInfo(`
+    ### Component Sandbox
 
+    This project is an ongoing **learning exercise**. It is NOT meant to be an official library.
 
-    ### Notes
+    I am recreating certain Element UI components to learn more about the following technologies:
 
-    Hello world!:
+    - React
+    - TypeScript
+    - Styled Components
+    - Storybook
+    - Jest
 
-    ### Usage
-    ~~~js
-    <div>This is an example component</div>
-    ~~~
-
-    ### To use this Storybook
-
-    Explore the panels on the left.
+    The offical Element UI library is here: https://elemefe.github.io/element-react/#/en-US/quick-start
   `)
 );
