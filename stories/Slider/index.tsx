72611208b7f092489d63ab28a0502edac3d516ad
Raymond Chen <rchen1992@gmail.com>
2019-07-14 18:05:09 -0700

    Slider component: min max story.

--- stories/Slider/index.tsx
+++ stories/Slider/index.tsx
@@ -1,8 +1,10 @@
 import { storiesOf } from '@storybook/react';
 import Basic from './Basic';
+import MinMax from './MinMax';
 
 const stories = storiesOf('Components/Slider', module) as any;
 
 Basic(stories);
+MinMax(stories);
 
 export default stories;
